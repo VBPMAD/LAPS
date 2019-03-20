6503a3e7cb3f974e7ba0c9e6e6522f64f9f64403
VBPMAD <38729527+VBPMAD@users.noreply.github.com>
2019-03-19 19:46:56 -0600

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,7 @@
+function displayCode() {
+    var valueOne = loader.engine.document.getElementById( 51651092).getProperty('value.value');
+   
+    loader.engine.document.getElementById(51761560).setValue(({"value": 54}));
+}
+    
+window.onchange = joinValues;
