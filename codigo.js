935ac87ba161c35253d69974cf6c6c70e9bde7d3
VBPMAD <38729527+VBPMAD@users.noreply.github.com>
2019-03-19 20:23:40 -0600

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -2,7 +2,7 @@ function displayCode() {
     var valueOne = loader.engine.document.getElementById(51651092).getProperty('value.value');
     console.log(5 + 6);
    console.log(valueOne);
-    loader.engine.document.getElementById(51761560).setValue(({"value": 54}));
+    loader.engine.document.getElementById(51255587).setValue(({"value": 54}));
 }
     
 window.onchange = displayCode;
