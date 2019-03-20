82a5348d7af38a082616f72486d4934802b11625
VBPMAD <38729527+VBPMAD@users.noreply.github.com>
2019-03-20 12:48:38 -0600

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,11 +1,13 @@
-loader.engine.document.getElementById(51761560).onchange = function() {displayCode()};
-
 function displayCode() {
-    
     /*
-    var valueOne = loader.engine.document.getElementById(51651092).getProperty('value.value');
-    console.log(5 + 6);
-   console.log(valueOne);
-    loader.engine.document.getElementById(51255587).setValue(({"value": 54}));*/
+    var valueOne = loader.engine.document.getElementById( 51651092).getProperty('value.value');
+if (valueOne = "ARGENTINA") then
+   
+    loader.engine.document.getElementById(51761560).setValue(({"value": 54}));
+
+end if
+*/
     alert("change");
 }
+    
+window.onchange = displayCode;
