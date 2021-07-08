275434c2a8d5bb965457d5576c5b956bf9f01e5b
VBPMAD <38729527+VBPMAD@users.noreply.github.com>
2021-07-08 14:02:03 -0500

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -7,13 +7,18 @@ function displayCode() {
        case "ANTIGUA": valueCode = "1"; break;
        case "ARUBA":valueCode = "297"; break;
 case "BAHAMAS":valueCode = "1"; break;
+case "BARBADOS":valueCode = "1"; break;
 case "CAYMAN ISLANDS":valueCode = "1"; break;
 case "CURASAO":valueCode = "599"; break;
+case "GRENADA":valueCode = "1"; break;
 case "HAITI":valueCode = "509"; break;
+case "JAMAICA":valueCode = "1"; break;
+case "ST. MAARTEN":valueCode = "599"; break;
 case "SURINAME":valueCode = "597"; break;
 case "TRINIDAD & TOBAGO":valueCode = "868"; break;
 default: valueCode = ""; break;
 }
+
 
        loader.engine.document.getElementById(97763832).setValue(({"value": valueCode}));
   }
